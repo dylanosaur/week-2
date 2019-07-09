3af16a738b89e0c23749688d97633806ba54b501
Dylan Adams <dadams10642@outlook.com>
2019-07-09 09:54:00 -0400

    w/e

---
+++ 0-outline.js
@@ -0,0 +1,35 @@
+/**
+ * REST API
+ * Express
+ * QC Prep
+ * QC @ 2:00
+ * Jest / Unit Testing
+ * 
+ */
+
+ /**
+  * REpresentational State Transfer
+  * state refers to the fields/data of some object, REST calls this a resource
+  * The types of operations a REST covers is called CRUD
+  * Create Read Update Delete
+  * 
+  * 
+  * A request:
+  * URL
+  * Headers: key-value pairs providing metadata about the request
+  * Body: content should be empty for GET requests
+  * we can characterize the various HTTP methods as safe and/or idempotent or neither
+  * safe: doesn't change any data server-side
+  * idempotent: repeated operation is the same as a single operation
+  * 
+  * A response
+  * 
+  * Headers: 
+  * Body:
+  * HTTP status code: categorical by hundreds
+  *     100's: informational
+  *     200's: success
+  *     300's: redirect
+  *     400's: client errors
+  *     500's: server errors
+  */
